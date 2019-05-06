0d3c8a02aa3cda1bf1264ec3b9e474797fd19d79
Matteo Barbieri <neopard@gmail.com>
2019-05-06 12:30:36 +0200

    Add fake generated data

--- fondo_pensioni/pages.py
+++ fondo_pensioni/pages.py
@@ -2,6 +2,8 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
+import random
+
 
 class Introduction(Page):
 
@@ -13,8 +15,47 @@ class Introduction(Page):
 
         return self.round_number == 1
 
+def get_fake_hc_series(round_number):
+
+    values_price = list()
+    values_prediction = list()
+
+    # for i in range(round_number-1):
+    for i in range(20):
+        values_price.append(random.randint(1, 100))
+        values_prediction.append(random.randint(1, 100))
+
+    # for i in range(round_number-1):
+    for i in range(5):
+        values_price.append(None)
+        values_prediction.append(None)
+
+    series_price = {
+        'name': "Price",
+        'data': values_price
+    }
+
+    series_prediction = {
+        'name': "Prediction",
+        'data': values_prediction
+    }
+
+    return [series_price, series_prediction]
+
 class Investi(Page):
-    pass
+
+    form_model = 'player'
+    form_fields = ['investimento']
+
+
+    def vars_for_template(self):
+        highcharts_series = get_fake_hc_series(self.round_number)
+
+        return {
+            'player_in_previous_rounds': self.player.in_previous_rounds(),
+            'highcharts_series': highcharts_series
+        }
+
 
 
 class ResultsWaitPage(WaitPage):
@@ -28,7 +69,7 @@ class Results(Page):
 
 
 page_sequence = [
-    Introduction,
+    # Introduction,  # TODO disabilitata per debug
     Investi,
     ResultsWaitPage,
     Results
