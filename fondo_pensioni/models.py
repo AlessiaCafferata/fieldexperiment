428ba93264718ab4264b8e83d7344d663bfb55a4
Matteo Barbieri <neopard@gmail.com>
2019-05-06 14:33:37 +0200

    Use correct data to build graph and table

--- fondo_pensioni/models.py
+++ fondo_pensioni/models.py
@@ -14,7 +14,8 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'fondo_pensioni'
     players_per_group = None
-    num_rounds = 4  # TODO deve essere 100
+    # num_rounds = 4  # TODO deve essere 100
+    num_rounds = 100  # TODO deve essere 100
     instructions_template = "fondo_pensioni/Instructions.html"
 
 
@@ -23,11 +24,23 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    pass
+
+    def set_price(self):
+
+        player = self.get_players()[0]
+        print(player)
+
+        player.price = 75
+
+        print("In set_price:")
 
 
 class Player(BasePlayer):
 
+    price = models.CurrencyField(
+        min=0,
+        # label="Quanto vuoi investire?"
+    )
 
     investimento = models.CurrencyField(
         min=0,
--- fondo_pensioni/pages.py
+++ fondo_pensioni/pages.py
@@ -49,16 +49,67 @@ class Investi(Page):
     form_fields = ['investimento']
 
 
+    def build_series(self):
+
+        values_price = list()
+        values_prediction = list()
+
+        # for i in range(round_number-1):
+        # for i in range(95):
+            # values_price.append(random.randint(1, 100))
+            # values_prediction.append(random.randint(1, 100))
+
+        rn = 1
+        for p in self.player.in_previous_rounds():
+
+            values_prediction.append(p.investimento)
+
+            if rn > (self.round_number - 2):
+                values_price.append(None)
+            else:
+                values_price.append(p.price)
+
+            rn += 1
+
+        # for i in range(round_number-1):
+        for i in range(Constants.num_rounds - rn):
+            values_price.append(None)
+            values_prediction.append(None)
+
+        series_price = {
+            'name': "Price",
+            'data': values_price
+        }
+
+        series_prediction = {
+            'name': "Prediction",
+            'data': values_prediction
+        }
+
+        return [series_prediction, series_price]
+
     def vars_for_template(self):
-        highcharts_series = get_fake_hc_series(self.round_number)
+        # highcharts_series = get_fake_hc_series(self.round_number)
+        highcharts_series = self.build_series()
 
         series_df = pd.DataFrame(columns=['Round', 'Prediction', 'Price'])
         series_df['Prediction'] = highcharts_series[0]['data']
         series_df['Price'] = highcharts_series[1]['data']
-        series_df['Round'] = range(1, 101)
+
+        print(series_df.shape)
+        print(len(range(1, Constants.num_rounds + 1)))
+
+        # Numero di round totali: 100 - 1
+        series_df['Round'] = range(1, Constants.num_rounds)
 
         # TODO debug, to remove
-        series_df = series_df.head(20)
+        # series_df = series_df.head(20)
+
+        # rn = 24
+        rn = self.round_number
+        series_df = series_df[series_df['Round'] < rn]
+        series_df = series_df[series_df['Round'] >= rn-20]
+
 
         # print(series_df)
 
@@ -74,7 +125,7 @@ class Investi(Page):
 class ResultsWaitPage(WaitPage):
 
     def after_all_players_arrive(self):
-        pass
+        self.group.set_price()
 
 
 class Results(Page):
