aca0cae5d5e623c13d6e6282e285ccb4bbb39d45
Matteo Barbieri <neopard@gmail.com>
2019-05-16 16:46:36 +0200

    Fix visualization bugs

--- fondo_pensioni/pages.py
+++ fondo_pensioni/pages.py
@@ -1,4 +1,3 @@
-from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
@@ -95,33 +94,31 @@ class QuestionarioWaitPage(WaitPage):
 
                 qf.write("\n")
 
-# class Contribute(Page):
-    # form_model = 'player'
-    # form_fields = ['contribution']
 
 def build_series(player, group, round_number):
 
     values_price = list()
-    values_prediction = list()
-
+    # values_prediction = list()
+    values_prediction = [None]
 
     player_in_previous_rounds = player.in_previous_rounds()
     group_in_previous_rounds = group.in_previous_rounds()
 
+    # for i in range(len(player_in_previous_rounds)):
+        # p = player_in_previous_rounds[i]
+        # print("Round {}: c: {}".format(i+1, p.contribution))
+
     for i in range(len(player_in_previous_rounds)):
-        rn = i + 1
 
         p = player_in_previous_rounds[i]
         g = group_in_previous_rounds[i]
 
         values_prediction.append(p.contribution)
+        values_price.append(g.price)
 
-        if rn > (round_number - 2):
-            values_price.append(None)
-        else:
-            values_price.append(g.price)
+    values_price.append(None)
 
-    # Serve per dopo
+    # Il turno attuale
     rn = len(player_in_previous_rounds) + 1
 
     # for i in range(round_number-1):
@@ -138,6 +135,9 @@ def build_series(player, group, round_number):
         'name': "Previsione",
         'data': values_prediction
     }
+
+
+    # print(len(series_price['data']))
 
     return [series_prediction, series_price]
 
@@ -157,24 +157,60 @@ class Investi(Page):
         series_df['Prezzo'] = highcharts_series[1]['data']
 
         # print(series_df.shape)
-        # print(len(range(1, Constants.num_rounds)))
+        # print(len(range(2, Constants.num_rounds + 1)))
 
         # Numero di round totali:
-        series_df['Periodo'] = range(1, Constants.num_rounds)
+        series_df['Periodo'] = range(1, Constants.num_rounds + 1)
+
+        # Taglia la prima riga relativa al periodo 1
+        series_df = series_df.iloc[1:, :]
+
+        # print(series_df)
 
         rn = self.round_number
-        series_df = series_df[series_df['Periodo'] < rn]
-        series_df = series_df[series_df['Periodo'] >= rn-20]
+        series_df = series_df[series_df['Periodo'] <= rn]
+        series_df = series_df[series_df['Periodo'] > rn-20]
+
+        # Sostituire "None" con "" (stringa vuota)
+        series_df_fixed = series_df.copy()
+        mask = series_df.applymap(lambda x: x is None)
+        cols = series_df.columns[(mask).any()]
+        for col in series_df_fixed[cols]:
+            series_df_fixed.loc[mask[col], col] = ''
 
         return {
             'player_in_previous_rounds': self.player.in_previous_rounds(),
             'highcharts_series': highcharts_series,
             'interest_rate': "{:.2f}%".format(Constants.R*100),
             'mean_dividend': "{:.2f}%".format(Constants.D),
-            'series_df_html': series_df.to_html(
-                index=False, classes=['table', 'table-sm'])
+            'series_df_html': series_df_fixed.to_html(
+                index=False, classes=['table', 'table-sm'], na_rep="")
         }
 
+
+def compute_price(contributions):
+    """
+    Compute the price at round t, given the contributions of players
+    """
+
+    mean_contribution = float(sum(contributions))/Constants.players_per_group
+    price = float(mean_contribution + Constants.D)/(1 + Constants.R)
+
+    return price
+
+
+def compute_payoff(player_prediction, price):
+    """
+    Calcola il guadagno
+    """
+
+    coefficient = (price - player_prediction)**2
+
+    payoff = max(
+        Constants.S - (( Constants.S/Constants.F)*coefficient),
+        0)
+
+    return payoff
 
 class ResultsWaitPage(WaitPage):
 
@@ -184,45 +220,23 @@ class ResultsWaitPage(WaitPage):
         players = group.get_players()
 
         contributions = [p.contribution for p in players]
-        group.mean_contribution = float(sum(contributions))/Constants.players_per_group
-        group.price = float(group.mean_contribution + Constants.D)/(1 + Constants.R)
+
+        group.price = compute_price(contributions)
 
         if self.round_number > 1:
 
-            group_in_previous_rounds = group.in_previous_rounds()
-
-            # Il gruppo al round precedente
-            group_in_previous_round = group_in_previous_rounds[len(group_in_previous_rounds)-1]
-
             # TODO da cancellare?
             for p in players:
-                # p.guess = p.contribution
 
-                if group.price == p.contribution:
-                    p.payoff = Constants.S
-                else:
+                player_in_previous_rounds = p.in_previous_rounds()
 
-                    # if (group_in_previous_round.price - p.contribution) == 0:
-                        # print(20*"#")
-                        # print(20*"#")
-                        # print(20*"#")
-                        # print(group_in_previous_round.price)
-                        # print(p.contribution)
+                # Il gruppo al round precedente p_tm1 = player at t minus 1
+                p_tm1 = player_in_previous_rounds[
+                    len(player_in_previous_rounds)-1]
 
-                    denominator = (
-                        Constants.F *
-                        # XXX Occhio, questo denominatore qualche
-                        # volta si azzera
-                        (group_in_previous_round.price - p.contribution)**2)
-
-                    # XXX occhio qui
-                    if denominator == 0:
-                        denominator = 0.1
-
-                    p.payoff = max(
-                        Constants.S - (
-                            Constants.S / denominator),
-                        0)
+                # Calcola il guadagno al turno corrente
+                payoff = compute_payoff(p_tm1.contribution, group.price)
+                p.payoff = payoff
 
         else:
             for p in players:
@@ -235,11 +249,11 @@ class FinalResults(Page):
         return self.round_number == Constants.num_rounds
 
 page_sequence = [
-    Introduction,
-    Controllo1,
-    Controllo2,
-    Questionario,
-    QuestionarioWaitPage,
+    # Introduction,
+    # Controllo1,
+    # Controllo2,
+    # Questionario,
+    # QuestionarioWaitPage,
     Investi,
     ResultsWaitPage,
     FinalResults,
